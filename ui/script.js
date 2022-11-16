af4dbad2fff1d2f38f345c54c6851870765722fd
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-11-17 00:01:49 +0100

    feat: character creator ui

--- ui/script.js
+++ ui/script.js
@@ -11,8 +11,33 @@ $(function() {
     });
 
     $(".interactionButton").click(function() {
+        const action = $(this).data("action");
+
+        if (action == "new") {
+            $(".overlay").fadeOut("fast");
+            $("#creationForm").fadeIn("fast");
+            return;
+        };
+
         $.post(`https://${GetParentResourceName()}/action`, JSON.stringify({
-            action: $(this).data("action")
+            action: action
         }));
+    });
+
+    $(".creationCancel").click(function() {
+        $(".overlay").fadeIn("fast");
+        $("#creationForm").fadeOut("fast");
+    });
+
+    $("#creationForm").submit(function() {
+        $.post(`https://${GetParentResourceName()}/action`, JSON.stringify({
+            action: "new",
+            firstName: $("#characterFN"),
+            lastName: $("#characterLN"),
+            dob: $("#characterDOB"),
+            gender: $("#characterGender")
+        }));
+        $("#characterFN, #characterLN, #characterDOB, #characterGender").val("")
+        return false;
     });
 });
