3df140215af047d29622e74590f888b2ae9db6b0
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-11-16 21:17:09 +0100

    ui files

--- fxmanifes.lua
+++ fxmanifes.lua
@@ -8,6 +8,13 @@ fx_version "cerulean"
 game "gta5"
 lua54 "yes"
 
+files {
+	"ui/index.html",
+	"ui/script.js",
+	"ui/style.css"
+}
+ui_page "ui/index.html"
+
 shared_script "config.lua"
 server_script "server.lua"
 client_script "client.lua"
