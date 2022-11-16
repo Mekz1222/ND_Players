27b023510ec44d8c93c3163c7a0d8ccbea2310b8
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-11-16 21:15:04 +0100

    files

---
+++ fxmanifes.lua
@@ -0,0 +1,13 @@
+-- For support join the discord: https://discord.gg/Z9Mxu72zZ6
+
+author "Andy & Geneva"
+description "Simple character selection for ND Core"
+version ""
+
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+shared_script "config.lua"
+server_script "server.lua"
+client_script "client.lua"
