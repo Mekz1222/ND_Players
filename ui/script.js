39f42b9b3532ca58909507cb6e1b208eb2ca4a52
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-11-17 05:39:36 +0100

    style: ui changes

--- ui/script.js
+++ ui/script.js
@@ -10,12 +10,17 @@ $(function() {
         };
     });
 
+    // interaction buttons at the bottom, can be left, right, new, delete.
     $(".interactionButton").click(function() {
         const action = $(this).data("action");
 
         if (action == "new") {
             $(".overlay").fadeOut("fast");
             $("#creationForm").fadeIn("fast");
+            return;
+        } else if (action == "delete") {
+            $(".overlay").fadeOut("fast");
+            $(".confirmDelete").fadeIn("fast");
             return;
         };
 
@@ -24,11 +29,28 @@ $(function() {
         }));
     });
 
+    // cancel character creator
     $(".creationCancel").click(function() {
         $(".overlay").fadeIn("fast");
         $("#creationForm").fadeOut("fast");
     });
 
+    // cancel character delete
+    $(".cancelDeleteBtn").click(function() {
+        $(".overlay").fadeIn("fast");
+        $(".confirmDelete").fadeOut("fast");
+    });
+
+    // confirm character delete
+    $(".confirmDeleteBtn").click(function() {
+        $(".overlay").fadeIn("fast");
+        $(".confirmDelete").fadeOut("fast");
+        $.post(`https://${GetParentResourceName()}/action`, JSON.stringify({
+            action: "delete"
+        }));
+    });
+
+    // confirm character creator
     $("#creationForm").submit(function() {
         $.post(`https://${GetParentResourceName()}/action`, JSON.stringify({
             action: "new",
