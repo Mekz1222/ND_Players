3b38e0f01bb9e1fa7761280e33f638833f35a2ee
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2023-04-03 00:31:00 +0200

    fix: old create character

--- server/main.lua
+++ server/main.lua
@@ -2,7 +2,9 @@ NDCore = exports["ND_Core"]:GetCoreObject()
 
 RegisterNetEvent("ND_CharactersV2:createCharacter", function(data)
     local source = source
-    NDCore.Functions.CreateCharacter(source, data.firstName, data.lastName, data.dob, data.gender, "", "CIV")
+    NDCore.Functions.CreateCharacter(source, data.firstName, data.lastName, data.dob, data.gender, function(characterId)
+        NDCore.Functions.SetActiveCharacter(source, characterId)
+    end)
 end)
 
 lib.callback.register("ND_CharactersV2:getCharacters", function(source)
