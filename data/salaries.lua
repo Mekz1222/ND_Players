6937e62b61fc62ff794c33b12c40846e60da9431
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2024-02-25 23:57:17 +0100

    feat: salaries

---
+++ data/salaries.lua
@@ -0,0 +1,22 @@
+return {
+    ["default"] = { -- default salary characters receive with none of these jobs.
+        enabled = true,
+        interval = 60,
+        amount = 75
+    },
+    ["mechanic"] = {
+        enabled = true,
+        interval = 60,
+        amount = 200
+    },
+    ["lspd"] = {
+        enabled = true,
+        interval = 60,
+        amount = 350
+    },
+    ["lsfd"] = {
+        enabled = true,
+        interval = 60,
+        amount = 350
+    }
+}
--- server/main.lua
+++ server/main.lua
@@ -9,6 +9,8 @@ local config = lib.load("data.configuration") or {
     }
 }
 
+config.salaries = lib.load("data.salaries") or {}
+
 NDCore.enableMultiCharacter(true)
 
 RegisterNetEvent("ND_Players:updateBucket", function(update)
@@ -65,4 +67,52 @@ end)
 
 lib.callback.register("ND_Players:fetchCharacters", function(source)
     return NDCore.fetchAllCharacters(source)
+end)
+
+local function getSalary(player)
+    if not player or not player.job then return end
+    
+    local job = player.job:lower()
+    for name, info in pairs(config.salaries) do
+        if info.enabled and name:lower() == job then
+            return info
+        end
+    end
+end
+
+CreateThread(function()
+    local payChecks = false
+    for _, salary in pairs(config.salaries) do
+        if salary.enabled then
+            payChecks = true
+            break
+        end
+    end
+
+    local lastSalaryPayouts = {}
+    while payChecks do
+        Wait(60000)
+
+        local time = os.time()
+        for _, player in pairs(NDCore.getPlayers()) do
+            local salaryInfo = getSalary(player) or config.salaries["default"] or config.salaries["DEFAULT"]
+            local src = player.source
+            local lastPayout = lastSalaryPayouts[src]
+
+            if not lastPayout then
+                lastSalaryPayouts[src] = time -- this will make it to where it won't pay the player until next interval which will prevent pay if switching characters everytime.
+            elseif salaryInfo and (not lastPayout or time-lastPayout > salaryInfo.interval*60) then                
+                local salary = salaryInfo.amount or 100
+                player.addMoney("bank", salary, "Salary")
+                player.notify({
+                    title = "Salary",
+                    description = ("Received $%d."):format(salary),
+                    type = "success",
+                    icon = "sack-dollar"
+                })
+                lastSalaryPayouts[src] = time
+            end
+
+        end
+    end
 end)
